b3e45b0d35c2317e701468d7af6cc5910212a205
Sahin Uzun <sahinuzun03@gmail.com>
2023-02-07 17:21:46 +0300

    Vuex store state kullanımı ile veri çekme

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,12 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    todoList : [
+      {id:1,todo:'Ders Çalış'},
+      {id:2,todo:'Film İzle'},
+      {id:3,todo:'Yemek Ye'},
+      {id:4,todo:'Koşu Yap'},
+  ],
   },
   getters: {
   },
