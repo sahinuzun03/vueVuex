34f67df8779564f0755bba832a2a557aef02937e
Sahin Uzun <sahinuzun03@gmail.com>
2023-02-07 17:22:44 +0300

    vuex store state management kullanımı

---
+++ src/components/List.vue
@@ -0,0 +1,26 @@
+<template>
+    <ul>
+        <li v-for="todo in todoList" :key="todo.id">{{ todo.todo }}</li>
+    </ul>
+</template>
+
+<script>
+import { useStore } from 'vuex'; //Store içerisine ulaşamak için kullanıyorum
+import { computed } from 'vue';
+
+export default {
+    setup(){
+        const store = useStore();
+
+        const todoList = computed(() => {
+            return store.state.todoList; //store içerisine bulunan state'te bulunan verilere ulaştım.
+        });
+
+        return {todoList}
+    }
+};
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    <!-- List içerisinde bulunan :todos props'a veri gönderiyorum burada ulunan todoList'i -->
+    <List></List>
+    <AddTodo/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import {ref,onUpdated} from 'vue'
+import List from '../components/List.vue'
+import AddTodo from '../components/AddTodo.vue'
+import {useRoute} from 'vue-router'
+export default {
+  name: 'Home',
+  components: {List,AddTodo},
+  setup(){
+  }
+}
+</script>
+
+
+
+
+
+
+
+<style>
+/* Vuex bize bir bilgiyi alt componentlere iletmek için bize bir alan tanımlar ve ihtiyacı olan komponentler buradan bu bilgiye ulaşıp değiştirmek isterlerse değiştirirler. */
+
+
+</style>
